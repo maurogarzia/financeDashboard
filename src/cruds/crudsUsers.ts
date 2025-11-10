abed6be8756ef31bfc6a02518cc32cfa2b084c32
maurogarzia <maurogarzia2@gmail.com>
2025-11-10 14:09:57 -0300

    I add crudUsers

---
+++ src/cruds/crudsUsers.ts
@@ -0,0 +1,35 @@
+import axios from "axios";
+import { ErrorAlert } from "../utils/ErrorAlert"
+import { BASE_URL } from "../utils/constantes";
+
+const URL_USERS = `${BASE_URL}/users`
+
+export const getAllUsers = async() => {
+    try {
+        const users = await axios.get(URL_USERS)
+        return users.data
+    } catch (error: any) {
+        console.log(error.message);
+        ErrorAlert('Ups!', 'No se pudo obtener los usuarios')
+    }
+}
+
+export const getUserById = async(id: string) => {
+    try {
+        const user = await axios.get(`${URL_USERS}/${id}`)
+        return user.data
+    } catch (error: any) {
+        console.log(error.mesage);
+        ErrorAlert('Ups!', 'No se pudo encontrar el usuario')
+    }
+}
+
+export const deleteUser = async(id : string) => {
+    try {
+        const deletedUser = await axios.delete(`${URL_USERS}/${id}`)
+        return deletedUser.data
+    } catch (error: any) {
+        console.log(error.message);
+        ErrorAlert('Ups!', 'No se pudo eliminar el usuario')
+    }
+}
